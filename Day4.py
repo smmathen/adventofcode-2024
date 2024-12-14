5e259b16c146347d33f3b33e3388be10fe625b77
Shawn Mathen <sgmathen@gmail.com>
2024-12-13 23:58:15 -0600

    part 2 complete why was this one so easy relative to other part 2s

--- Day4.py
+++ Day4.py
@@ -33,7 +33,6 @@ def part1():
                     count += 1
                 # down left
                 if j >= 3:
-                    print(i, j)
                     if graph[i+1][j-1] == 'M' and graph[i+2][j-2] == 'A' and graph[i+3][j-3] == 'S':
                         count += 1
                 # down right
@@ -52,4 +51,29 @@ def part1():
     print("Count:", count)
 
 
+def part2():
+    count = 0
+    f = open("ainput.txt", 'r')
+    lines = f.readlines()
+    graph = []
+    for line in lines:
+        chars = list(line)[:-1]
+        graph.append(chars)
+
+    for i in range(len(graph)):
+        for j in range(len(graph[i])):
+            if graph[i][j] != "A":
+                continue
+
+            # check top left and bottom right
+            if i > 0 and j > 0 and i < len(graph)-1 and j < len(graph[i])-1:
+                if (graph[i-1][j-1] == 'M' and graph[i+1][j+1] == 'S') or (graph[i-1][j-1] == 'S' and graph[i+1][j+1] == 'M'):
+                    # then can check top right and bottom left
+                    if (graph[i-1][j+1] == 'M' and graph[i+1][j-1] == 'S') or (graph[i-1][j+1] == 'S' and graph[i+1][j-1] == 'M'):
+                        count += 1
+
+    print("Count:", count)
+
+
 part1()
+part2()
